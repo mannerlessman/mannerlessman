4b0d52fa6a7db83dab6a87094f0bb4df409ea8ae
mannerlessman <mannerlessman@github.com>
2019-10-01 16:13:28 +0530

    i have changed nothing

---
+++ app/src/main/java/com/blackbaba/youtubedemo/Config.java
@@ -0,0 +1,10 @@
+package com.blackbaba.youtubedemo;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyD058lekLpVb8_j8Dvl1MLLpnGscCmnwlc";
+
+}
